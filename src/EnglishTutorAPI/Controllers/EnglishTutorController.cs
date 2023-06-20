db87ecd41d85567d027d3d356ae0a074c1223edb
Felipe-Santana1 <felipe.estudos666@outlook.com>
2023-06-20 07:43:53 -0300

    Integração ChatGPt

---
+++ src/EnglishTutorAPI/Controllers/EnglishTutorController.cs
@@ -0,0 +1,46 @@
+﻿using EnglishTutorAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnglishTutorAPI.Controllers
+{
+    [ApiController]
+    [Route("api/english-tutor")]
+    public class EnglishTutorController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public EnglishTutorController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration)
+        {
+            var token = "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";
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                 new AuthenticationHeaderValue("Bearer", token);
+
+            //MONTANDO RQUISIÇÃO
+            var model = new ChatGptInputModel(text);
+            var requestBody = JsonSerializer.Serialize(model);
+            var context = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            //ENVIANDO REQUISIÇÃO
+            var response =
+                await _httpClient.PostAsync("https://api.openai.com/v1/completions", context);
+
+            //RECEBENDO RESPOSTA DA REQUISIÇÃO
+            var result = await response.Content.ReadFromJsonAsync<ChatGptViewModel>();
+            var promptResponse = result.choices.First();
+
+            return Ok(promptResponse);
+        }
+    }
+}
---
+++ src/EnglishTutorAPI/Models/ChatGptInputModel.cs
@@ -0,0 +1,21 @@
+﻿namespace EnglishTutorAPI.Models
+{
+    public class ChatGptInputModel
+    {
+        public ChatGptInputModel(string prompt)
+        {
+
+            //INICIANDO MODELO DE ENTRADA && DANDO FRASE DE NORTE
+            this.prompt = $"Correct this English phrase: {prompt}";
+            temperature = 0.2m;
+            max_tokens = 100;
+            model = "text-davinci-003";
+        }
+
+        public string model { get; set; } //Modelo Fixo
+        public string prompt { get; set; } //Texto que iremos enviar
+        public int max_tokens { get; set; } //Tokens maximos para ser gerado
+        public decimal temperature { get; set; } //Valores maiores saidas amplas
+
+    }
+}
